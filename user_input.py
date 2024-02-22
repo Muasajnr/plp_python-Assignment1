71ebece87e5fcc953ada363037834154d7260eb1
Muasa Jnr <116666398+Muasajnr@users.noreply.github.com>
2024-02-22 14:06:17 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# storing name input
+name = input("Enter Your Name: ")
+
+# storing age input and converting it to an integer
+age = int(input('Enter Your Age: '))
+
+# storing location input
+location = input("Enter Place of Residence: ")
+
+# display
+print(f"Hello {name}, you are {age} years old and live in {location}")
+
